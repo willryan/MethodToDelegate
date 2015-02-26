0cb93aef0c09ced0457f9ad3a3f65d846b5b37a7
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2015-02-26 16:15:43 -0500

    add nuget package

--- MethodToDelegate/Properties/AssemblyInfo.cs
+++ MethodToDelegate/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MethodToDelegate")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Create Delegates from MethodInfos")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Atomic Object")]
 [assembly: AssemblyProduct("MethodToDelegate")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
